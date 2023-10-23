3d1e36f0091061711f5edcc0b78c738d90ce4bae
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-10-23 14:21:26 -0400

    Getting inputs

--- js/getTracks.js
+++ js/getTracks.js
@@ -1,4 +1,12 @@
 
 function getInputs() {
-    // TODO: Store the inputs in some sort of data structure
+    // TODO: Validate inputs
+    const params = ['acoustic', 'dance', 'live', 'energy',
+        'valence', 'tempo', 'popularity', 'category', 'genre', 'playlist'];
+    const param_map = new Map();
+    for (const param of params) {
+        let val = document.getElementById(param).value;
+        param_map.set(param, val);
+    }
+    console.log(param_map);
 }
